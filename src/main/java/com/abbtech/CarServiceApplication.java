b961727204d134296b37914b7b5f1975188b4d0d
Anar Arabli <anar.arabli@gmail.com>
2026-02-08 21:28:05 +0400

    Security

---
+++ src/main/java/com/abbtech/CarServiceApplication.java
@@ -0,0 +1,14 @@
+package com.abbtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(CarServiceApplication.class, args);
+    }
+
+
+}
